943e3ce2510f6e4beee4e032e72258e873d9c380
Matheus Oliveira <matheus_obarbosa@hotmail.com>
2017-05-18 11:36:34 -0300

    initial commit

---
+++ SAC2017/app/src/main/java/br/edu/ufcg/sac2017/ProgramationActivity.java
@@ -0,0 +1,17 @@
+package br.edu.ufcg.sac2017;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mathe on 18/05/2017.
+ */
+public class ProgramationActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.programation_activity);
+    }
+}
