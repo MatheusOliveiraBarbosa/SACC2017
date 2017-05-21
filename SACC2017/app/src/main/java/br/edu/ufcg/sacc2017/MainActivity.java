1769c4f701788ede49d82f5f204c89dd8beb0585
Matheus Oliveira <matheus_obarbosa@hotmail.com>
2017-05-21 18:22:40 -0300

    adicionado bottomNavigationBar com as 3 opçoes (ps: falta implementar os fragmentos de cada)

--- SACC2017/app/src/main/java/br/edu/ufcg/sacc2017/MainActivity.java
+++ SACC2017/app/src/main/java/br/edu/ufcg/sacc2017/MainActivity.java
@@ -1,7 +1,10 @@
 package br.edu.ufcg.sacc2017;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_favorites:
+
+                            case R.id.action_schedules:
+
+                            case R.id.action_music:
+
+                        }
+                        return true;
+                    }
+                });
 
     }
 }
